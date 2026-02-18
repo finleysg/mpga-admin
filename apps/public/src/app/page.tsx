e41d1343a53b48fc29ba3d5be6d2d80713af57ee
Stuart Finley <finleysg@gmail.com>
2026-02-17 20:02:29 -0600

    Constrain the hero section a bit.

--- apps/public/src/app/page.tsx
+++ apps/public/src/app/page.tsx
@@ -14,7 +14,7 @@ export default async function HomePage() {
 
 	return (
 		<main>
-			<section className="py-8">
+			<section className="mx-auto max-w-6xl px-4 py-8">
 				<HeroSlide type="logo" imageUrl="/images/mpga-logo.png" priority />
 			</section>
 			{aboutContent && <AboutSection title={aboutContent.title} content={aboutContent.content} />}
