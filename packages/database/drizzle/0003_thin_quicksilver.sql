b347168a3bd6dd5b4318c71c3e4555f406ddf4f8
Stuart Finley <finleysg@gmail.com>
2026-02-23 17:38:01 -0600

    Audit field migration.

---
+++ packages/database/drizzle/0003_thin_quicksilver.sql
@@ -0,0 +1,6 @@
+ALTER TABLE `club` ADD `updateDate` datetime(6);--> statement-breakpoint
+ALTER TABLE `club` ADD `updateBy` varchar(100);--> statement-breakpoint
+ALTER TABLE `clubContact` ADD `updateDate` datetime(6);--> statement-breakpoint
+ALTER TABLE `clubContact` ADD `updateBy` varchar(100);--> statement-breakpoint
+ALTER TABLE `contact` ADD `updateDate` datetime(6);--> statement-breakpoint
+ALTER TABLE `contact` ADD `updateBy` varchar(100);
